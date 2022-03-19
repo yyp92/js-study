8afb79fba41da8739375608a59e0667e69263999
yangyipeng <yangyipeng@tianyancha.com>
2022-03-19 14:15:56 +0800

    feat: 移除链表的值 - 2022-03-19

--- src/算法/链表/移除链表元素.js
+++ src/算法/链表/移除链表元素.js
@@ -12,10 +12,10 @@
  * 
  * 示例 3：
  * 输入：head = [7,7,7,7], val = 7
- * 输出：[]
+ * 输出：[]z
  */
 
- class LinkNode {
+class LinkNode {
     constructor(val, next) {
         this.val = val;
         this.next = next;
@@ -30,46 +30,6 @@ var MyLinkedList = function() {
     this._size = 0;
     this._tail = null;
     this._head = null;
-};
-
-/**
- * Get the value of the index-th node in the linked list. If the index is invalid, return -1. 
- * @param {number} index
- * @return {number}
- */
-MyLinkedList.prototype.getNode = function(index) {
-    if (index < 0 || index >= this._size) return null;
-
-    // 创建虚拟头节点
-    let cur = new LinkNode(0, this._head);
-
-    // 0 -> head
-    while(index-- >= 0) {
-        cur = cur.next;
-    }
-
-    return cur;
-};
-MyLinkedList.prototype.get = function(index) {
-    if (index < 0 || index >= this._size) return -1;
-
-    // 获取当前节点
-    return this.getNode(index).val;
-};
-
-/**
- * Add a node of value val before the first element of the linked list. After the insertion, the new node will be the first node of the linked list. 
- * @param {number} val
- * @return {void}
- */
-MyLinkedList.prototype.addAtHead = function(val) {
-    const node = new LinkNode(val, this._head);
-    this._head = node;
-    this._size++;
-
-    if (!this._tail) {
-        this._tail = node;
-    }
 };
 
 /**
@@ -91,65 +51,61 @@ MyLinkedList.prototype.addAtTail = function(val) {
     this._head = node;
 };
 
-/**
- * Add a node of value val before the index-th node in the linked list. If index equals to the length of linked list, the node will be appended to the end of linked list. If index is greater than the length, the node will not be inserted. 
- * @param {number} index 
- * @param {number} val
- * @return {void}
- */
-MyLinkedList.prototype.addAtIndex = function(index, val) {
-    if (index > this._size) return;
+// 实现逻辑
+const ListNode = (head = []) => {
+    const link = new MyLinkedList()
 
-    if (index <= 0) {
-        this.addAtHead(val);
-        return;
+    head.forEach(item => {
+        link.addAtTail(item)
+    })
+
+    return link._head
+}
+
+// 删除链表元素
+var removeElements = function(head, val) {
+    const ret = ListNode(head);
+    const temp = []
+    if (!ret) {
+        return []
     }
 
-    if (index === this._size) {
-        this.addAtTail(val);
-        return;
-    }
+    let cur = ret
 
-    // 获取目标节点的上一个的节点
-    const node = this.getNode(index - 1);
-    node.next = new LinkNode(val, node.next);
-    this._size++;
-};
-
-/**
- * Delete the index-th node in the linked list, if the index is valid. 
- * @param {number} index
- * @return {void}
- */
-MyLinkedList.prototype.deleteAtIndex = function(index) {
-    if (index < 0 || index >= this._size) return;
-
-    if (index === 0) {
-        this._head = this._head.next;
-        // 如果删除的这个节点同时是尾节点，要处理尾节点
-        if (index === this._size - 1){
-            this._tail = this._head
+    while(cur.next) {
+        if(cur.next.val === val) {
+            cur.next = cur.next.next;
+            continue;
         }
-        this._size--;
-        return;
-    }
-    // 获取目标节点的上一个的节点
-    const node = this.getNode(index - 1);    
-    node.next = node.next.next;
 
-    // 处理尾节点
-    if (index === this._size - 1) {
-        this._tail = node;
+        temp.push(cur.val)
+        cur = cur.next;
     }
 
-    this._size--;
+    // 解决所有链表都是一样的值
+    if (ret.val === val && !ret.next) {
+        return []
+    }
+
+    // 保证插入链表最后一项的值
+    temp.push(cur.val)
+
+    return temp;
 };
 
-// 测试
-const obj = new MyLinkedList()
-
 
 // 测试
+// 输入：head = [1,2,6,3,4,5,6], val = 6
+// 输出：[1,2,3,4,5]
+const head = [1,2,6,3,4,5,6]
+const val = 6
+console.log('====-----',JSON.stringify(removeElements(head, val)))
 
+const head1 = []
+const val1 = 1
+console.log('====-----',JSON.stringify(removeElements(head1, val1)))
 
+const head2 = [7,7,7,7]
+const val2 = 7
+console.log('====-----',JSON.stringify(removeElements(head2, val2)))
    
