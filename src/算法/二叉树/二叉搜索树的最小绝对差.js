93c63622ffae1de9668e2cd20f7737e54feef7af
yangyipeng001 <892973319@qq.com>
2022-10-25 21:28:02 +0800

    feat: 二叉树-验证二叉搜索树，二叉搜索树的最小绝对差

---
+++ src/算法/二叉树/二叉搜索树的最小绝对差.js
@@ -0,0 +1,86 @@
+/**
+ * 530.二叉搜索树的最小绝对差
+ * https://leetcode.cn/problems/minimum-absolute-difference-in-bst/
+ * 
+ * 给你一棵所有节点为非负值的二叉搜索树，请你计算树中任意两节点的差的绝对值的最小值。
+ * 示例：
+ *  https://img-blog.csdnimg.cn/20201014223400123.png
+ * 
+ * 提示：树中至少有 2 个节点。
+ */
+
+// 递归 先转换为有序数组
+var getMinimumDifference = function (root) {
+    let arr = [];
+    const buildArr = (root) => {
+        if (root) {
+            buildArr(root.left);
+            arr.push(root.val);
+            buildArr(root.right);
+        }
+    }
+    buildArr(root);
+
+    let diff = arr[arr.length - 1];
+    for (let i = 1; i < arr.length; i++) {
+        if (diff > arr[i] - arr[i - 1]) {
+            diff = arr[i] - arr[i - 1];
+        }
+    }
+
+    return diff;
+}
+
+// 递归 在递归的过程中更新最小值
+var getMinimumDifference = function(root) {
+    let res = Infinity
+    let preNode = null
+
+    // 中序遍历
+    const inorder = (node) => {
+        if (!node) return
+
+        inorder(node.left)
+
+        // 更新res
+        if (preNode) res = Math.min(res, node.val - preNode.val)
+        // 记录前一个节点         
+        preNode = node
+
+        inorder(node.right)
+    }
+
+    inorder(root)
+    return res
+}
+
+// 迭代 中序遍历
+var getMinimumDifference = function(root) {
+    let stack = []
+    let cur = root
+    let res = Infinity
+    let pre = null
+
+    while(cur || stack.length) {
+        // 指针来访问节点，访问到最底层
+        if (cur) {
+            // 将访问的节点放进栈
+            stack.push(cur)
+
+            // 左
+            cur = cur.left
+        }
+        else {
+            cur = stack.pop()
+
+            // 中
+            if (pre) res = Math.min(res, cur.val - pre.val)
+            pre = cur
+
+            // 右
+            cur = cur.right
+        }
+    }
+
+    return res
+}
---
+++ src/算法/二叉树/验证二叉搜索树.js
@@ -0,0 +1,66 @@
+/**
+ * 98.验证二叉搜索树
+ * https://leetcode.cn/problems/validate-binary-search-tree/
+ * 
+ * 给定一个二叉树，判断其是否是一个有效的二叉搜索树。
+ * 假设一个二叉搜索树具有如下特征：
+ *  节点的左子树只包含小于当前节点的数。
+ *  节点的右子树只包含大于当前节点的数。
+ *  所有左子树和右子树自身必须也是二叉搜索树。
+ * https://img-blog.csdnimg.cn/20210203144334501.png
+ * 
+ * 此题用的是中序遍历
+ */
+
+// 辅助数组解决
+const isValidBST = function (root) {
+    let arr = [];
+
+    const buildArr = (root) => {
+        if (root) {
+            buildArr(root.left);
+
+            // 将二叉搜索树转换为有序数组
+            arr.push(root.val);
+
+            buildArr(root.right);
+        }
+    }
+    buildArr(root);
+
+    for (let i = 1; i < arr.length; i++) {
+        // 注意要小于等于，搜索树里不能有相同元素
+        if (arr[i] <= arr[i - 1]) return false;
+    }
+
+    return true;
+}
+
+// 递归中解决
+const isValidBST1 = function (root) {
+    let pre = null;
+    // 用来记录前一个节点
+
+    const inOrder = (root) => {
+        if (root === null) {
+            return true;
+        }
+            
+        // 左
+        let left = inOrder(root.left);
+
+        if (pre !== null && pre.val >= root.val) {
+            return false;
+        }
+           
+        // 记录前一个节点
+        pre = root;
+
+        // 右
+        let right = inOrder(root.right);
+
+        return left && right;
+    }
+
+    return inOrder(root);
+}
