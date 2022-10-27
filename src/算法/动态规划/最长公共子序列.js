459c5bdfb933996489c66971e3c215035b606041
yangyipeng001 <892973319@qq.com>
2022-10-27 23:27:14 +0800

    feat: 动态规划-最长重复子数组，最长公共子序列

---
+++ src/算法/动态规划/最长公共子序列.js
@@ -0,0 +1,60 @@
+/**
+ * 1143.最长公共子序列
+ * https://leetcode.cn/problems/longest-common-subsequence/
+ * 
+ * 给定两个字符串 text1 和 text2，返回这两个字符串的最长公共子序列的长度。
+ * 一个字符串的 子序列 是指这样一个新的字符串：它是由原字符串在不改变字符的相对顺序的情况下删除某些字符（也可以不删除任何字符）后组成的新字符串。
+ * 例如，"ace" 是 "abcde" 的子序列，但 "aec" 不是 "abcde" 的子序列。两个字符串的「公共子序列」是这两个字符串所共同拥有的子序列。
+ * 若这两个字符串没有公共子序列，则返回 0。
+ * 
+ * 示例 1:
+ *  输入：text1 = "abcde", text2 = "ace" 
+ *  输出：3 
+ *  解释：最长公共子序列是 "ace"，它的长度为 3。
+ * 
+ * 示例 2: 
+ *  输入：text1 = "abc", text2 = "abc" 
+ *  输出：3 
+ *  解释：最长公共子序列是 "abc"，它的长度为 3。
+ * 
+ * 示例 3: 
+ *  输入：text1 = "abc", text2 = "def" 
+ *  输出：0 
+ *  解释：两个字符串没有公共子序列，返回 0。
+ * 
+ * 提示:
+ *  1 <= text1.length <= 1000
+ *  1 <= text2.length <= 1000 输入的字符串只含有小写英文字符。
+ */
+ 
+const longestCommonSubsequence = (text1, text2) => {
+    let dp = Array.from(Array(text1.length + 1), () => Array(text2.length + 1).fill(0));
+
+    for (let i = 1; i <= text1.length; i++) {
+        for (let j = 1; j <= text2.length; j++) {
+            // 如果text1[i - 1] 与 text2[j - 1]相同，那么找到了一个公共元素，所以dp[i][j] = dp[i - 1][j - 1] + 1;
+            if (text1[i - 1] === text2[j - 1]) {
+                dp[i][j] = dp[i - 1][j - 1] + 1
+            }
+            // 如果text1[i - 1] 与 text2[j - 1]不相同，那就看看text1[0, i - 2]与text2[0, j - 1]的最长公共子序列 和 text1[0, i - 1]与text2[0, j - 2]的最长公共子序列，取最大的。
+            // 即：dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+            else {
+                dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1])
+            }
+        }
+    }
+
+    return dp[text1.length][text2.length];
+};
+
+
+// 测试
+const text1 = "abcde"
+const text11 = "ace" 
+const text2 = "abc"
+const text22 = "abc" 
+const text3 = "abc"
+const text33 = "def" 
+console.log(longestCommonSubsequence(text1, text11))
+console.log(longestCommonSubsequence(text2, text22))
+console.log(longestCommonSubsequence(text3, text33))
---
+++ src/算法/动态规划/最长重复子数组.js
@@ -0,0 +1,71 @@
+/**
+ * 718.最长重复子数组
+ * https://leetcode.cn/problems/maximum-length-of-repeated-subarray/
+ * 
+ * 给两个整数数组 A 和 B ，返回两个数组中公共的、长度最长的子数组的长度。
+ * 
+ * 示例：
+ *  输入： A: [1,2,3,2,1] B: [3,2,1,4,7] 
+ *  输出：3 
+ *  解释： 长度最长的公共子数组是 [3, 2, 1] 。
+ * 
+ * 提示：
+ *  1 <= len(A), len(B) <= 1000
+ *  0 <= A[i], B[i] < 100
+ */
+const findLength = (A, B) => {
+    // A、B数组的长度
+    const [m, n] = [A.length, B.length];
+
+    // dp数组初始化，都初始化为0
+    const dp = new Array(m + 1).fill(0).map(x => new Array(n + 1).fill(0));
+
+    // 初始化最大长度为0
+    let res = 0;
+    for (let i = 1; i <= m; i++) {
+        for (let j = 1; j <= n; j++) {
+            // 遇到A[i - 1] === B[j - 1]，则更新dp数组
+            if (A[i - 1] === B[j - 1]) {
+                dp[i][j] = dp[i - 1][j - 1] + 1;
+            }
+
+            // 更新res
+            res = dp[i][j] > res ? dp[i][j] : res;
+        }
+    }
+
+    // 遍历完成，返回res
+    return res;
+};
+
+// 滚动数组
+const findLength1 = (nums1, nums2) => {
+    let len1 = nums1.length
+    let len2 = nums2.length;
+
+    // dp[i][j]: 以nums1[i-1]、nums2[j-1]为结尾的最长公共子数组的长度
+    let dp = new Array(len2 + 1).fill(0);
+
+    let res = 0;
+    for (let i = 1; i <= len1; i++) {
+        for (let j = len2; j > 0; j--) {
+            if (nums1[i - 1] === nums2[j - 1]) {
+                dp[j] = dp[j - 1] + 1;
+            }
+            else {
+                dp[j] = 0;
+            }
+
+            res = Math.max(res, dp[j]);
+        }
+    }
+
+    return res;
+}
+
+
+// 测试
+const A = [1,2,3,2,1] 
+const B = [3,2,1,4,7] 
+console.log(findLength(A, B))
+console.log(findLength1(A, B))
